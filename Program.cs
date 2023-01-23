e9c864b9e0a2b444eb1c4a07d744194c7e9068ab
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-01-23 14:58:44 -0500

    Delete Test 2

--- Program.cs
+++ Program.cs
@@ -3,29 +3,14 @@
 2. Make the test pass(DO NOT CHANGE TEST!)
 3. Change the code (without breaking the test) aka refactoring
 */
+
+
 // TEST 1 (Testing throw sys method)
 try
 {
     throw new DivideByZeroException("Divide by zero");
 }
 catch (DivideByZeroException e)
-{
-
-}
-// TEST 2 (Testing try sys method)
-try
-{
-    try
-    {
-        throw new System.Exception("This exception should fail");
-        throw new DivideByZeroException("Divide by zero");
-    }
-    catch (DivideByZeroException e)
-    {
-
-    }
-}
-catch
 {
 
 }
@@ -38,7 +23,6 @@ try
     try
     {
         throw new System.InvalidTimeZoneException("This exception should fail");
-        throw new DivideByZeroException("Divide by zero");
     }
     catch (DivideByZeroException e)
     {
