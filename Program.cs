a5a90e6561a29b0f0a8339d34d8e85ab279b7fc8
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-01-23 14:35:55 -0500

    Make Test 2 Pass by adding a try catch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// TEST 1
+﻿/*
+1. Write a failing test(As close to zero changes to code)
+2. Make the test pass(DO NOT CHANGE TEST!)
+3. Change the code (without breaking the test) aka refactoring
+*/
+// TEST 1 (Testing throw sys method)
 try
 {
     throw new DivideByZeroException("Divide by zero");
@@ -7,6 +12,24 @@ catch (DivideByZeroException e)
 {
 
 }
+// TEST 2 (Testing try sys method)
+try
+{
+    try
+    {
+        throw new System.Exception("This exception should fail");
+        throw new DivideByZeroException("Divide by zero");
+    }
+    catch (DivideByZeroException e)
+    {
+
+    }
+}
+catch
+{
+
+}
+
 
 
 
