927de5fc1f5bb94b22cfddf72237c6ed34d18637
dnierman0920 <40249436+dnierman0920@users.noreply.github.com>
2023-01-25 11:47:50 -0500

    create new test CallRecordFailingTest - compile error b/c RecordFailingTest does not exist

--- Program.cs
+++ Program.cs
@@ -4,30 +4,41 @@
 3. Change the code (without breaking the test) aka refactoring
 */
 
+/*
+4 PARTS TO TEST FRAMEWORK
+1) Test Discovery
+2) Executer
+3) Asserter
+4) Result Summarizer
+*/
+
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Action[] tests = { Test1, Test3, Test4, Test5, Test6, Test7, CallPassedTestTwice };
+        Action[] tests = { Test1, Test3, Test4, Test5, Test6, Test7, CallPassedTestTwice, CallRecordFailingTest };
         ExecuteTests(tests);
-        // TEST Executor
+
+        //  Executor
         static void ExecuteTests(Action[] tests)
         {
-            foreach (Action test in tests)
+            foreach (System.Action test in tests)
             {
                 try
                 {
                     test();
+                    // test.Method.Name;
                 }
                 catch (Exception e2)
                 {
+                    // TestResults.RecordFailingTest
                     throw new System.Exception($"unexpected exception type {e2.GetType()}, {e2.ToString()}");
-
                 }
             }
 
         }
+        // Asserter
         static void Assert<TException>(Action test) where TException : Exception
         {
             try
@@ -43,11 +54,9 @@ internal class Program
             {
                 throw new System.Exception($"unexpected exception type {e2.GetType()}");
             }
-
-
         }
 
-
+        // Test Discovery
         static void Test1()
         {
             static void M()
@@ -66,7 +75,6 @@ internal class Program
             Assert<InvalidTimeZoneException>(M);
         }
 
-        // 
         static void Test4()
         {
             static void M()
@@ -76,9 +84,7 @@ internal class Program
             Assert<ArgumentException>(M);
         }
 
-
-
-        // Test it is printing the right thing
+        // testResults.Summarize is building the correct string to display passed count & failed count
         static void Test5()
         {
             System.IO.StringWriter sw = new System.IO.StringWriter();
@@ -94,59 +100,83 @@ internal class Program
                 throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
             }
         }
-    }
-    static void Test6()
-    {
-        System.IO.StringWriter sw = new System.IO.StringWriter();
-        var testResults = new TestResults();
-        testResults.CountPassedTest("random string", System.IO.TextWriter.Null);
-        testResults.Summarize(sw);
-        string expected = $"Passed#:{1} | Failed#:{0}\n";
-        if (string.Equals(expected, sw.ToString()))
-        {
 
-        }
-        else
+        // testResult.RecordPassingTest is incrementing passed test count by 1 (passedCount)
+        static void Test6()
         {
-            throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
-        }
-    }
-    static void Test7()
-    {
-        System.IO.StringWriter sw = new System.IO.StringWriter();
-        var testResults = new TestResults();
-        var nameOfTest = "TEST#";
-        testResults.CountPassedTest(nameOfTest, sw);
-        string expected = $"Passed:{nameOfTest}\n";
-        if (string.Equals(expected, sw.ToString()))
-        {
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            var testResults = new TestResults();
+            testResults.RecordPassingTest("random string", System.IO.TextWriter.Null);
+            testResults.Summarize(sw);
+            string expected = $"Passed#:{1} | Failed#:{0}\n";
+            if (string.Equals(expected, sw.ToString()))
+            {
 
+            }
+            else
+            {
+                throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
+            }
         }
-        else
-        {
-            throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
-        }
-    }
-    static void CallPassedTestTwice()
-    {
-        System.IO.StringWriter sw = new System.IO.StringWriter();
-        var testResults = new TestResults();
-        testResults.CountPassedTest("random string", System.IO.TextWriter.Null);
-        testResults.CountPassedTest("random string", System.IO.TextWriter.Null);
-        testResults.Summarize(sw);
-        string expected = $"Passed#:{2} | Failed#:{0}\n";
-        if (string.Equals(expected, sw.ToString()))
-        {
 
-        }
-        else
+        // testResults.RecordPassingTest is building correct string to display nameOfTest
+        static void Test7()
         {
-            throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            var testResults = new TestResults();
+            var nameOfTest = "TEST#";
+            testResults.RecordPassingTest(nameOfTest, sw);
+            string expected = $"Passed:{nameOfTest}\n";
+            if (string.Equals(expected, sw.ToString()))
+            {
+
+            }
+            else
+            {
+                throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
+            }
+        }
+
+        // testResult.RecordPassingTest is incrementing passed test count by 1 each time it is called (passedCount)
+        static void CallPassedTestTwice()
+        {
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            var testResults = new TestResults();
+            testResults.RecordPassingTest("random string", System.IO.TextWriter.Null);
+            testResults.RecordPassingTest("random string", System.IO.TextWriter.Null);
+            testResults.Summarize(sw);
+            string expected = $"Passed#:{2} | Failed#:{0}\n";
+            if (string.Equals(expected, sw.ToString()))
+            {
+
+            }
+            else
+            {
+                throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
+            }
+        }
+
+        // testResult.RecordFailingTest is incrementing passed test count by 1 (passedCount)
+        static void CallRecordFailingTest()
+        {
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            var testResults = new TestResults();
+            testResults.RecordFailingTest();
+            testResults.Summarize(sw);
+            string expected = $"Passed#:{1} | Failed#:{0}\n";
+            if (string.Equals(expected, sw.ToString()))
+            {
+
+            }
+            else
+            {
+                throw new System.Exception($"Strings did NOT match \n'{expected}'\n'{sw.ToString()}'");
+            }
         }
     }
 }
 
-
+// Result Summarizer
 class TestResults
 {
     int passedCount = 0;
@@ -154,12 +184,13 @@ class TestResults
     {
         writer.WriteLine($"Passed#:{passedCount} | Failed#:{0}");
     }
-    public void CountPassedTest(string testName, System.IO.TextWriter writer)
+    public void RecordPassingTest(string testName, System.IO.TextWriter writer)
     {
         writer.WriteLine($"Passed:{testName}");
         passedCount++;
     }
 }
+// bonus points print out the error message
 
 
 
